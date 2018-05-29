2c26b3e87807fcc163cbb0902013f936b36f5a5f
james <james80216@ymail.com>
2018-05-29 18:02:29 +0800

    指向非常數資料的非常數指標

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void bubbleSort( int *array,unsigned int size ); /*指向非常數資料的非常數指標*/
+
+int main(void) {
+
+	int a[SIZE]={1,3,5,7,9,2,4,6,8,10};
+	
+	unsigned int i; 
+	
+	puts(" Data items in original order");
+	    
+		for(i=0;i<SIZE;++i){
+			printf("%4d ",a[i]);
+		}
+		
+		bubbleSort(a, SIZE); 
+		
+		puts("\n Data items in ascending order");
+		
+		for(i=0;i<SIZE;++i){
+			printf("%4d ",a[i]);
+	    }
+		
+		puts("");
+}
